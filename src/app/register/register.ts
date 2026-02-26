4a94d015ef8aba2aa05de1a7481914684e0b54a4
Jonas Jason Bondoc <jjbondoc07@gmail.com>
2026-02-26 20:06:02 +0800

    Initial commit - Angular Registration Form

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -12,6 +19,9 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-register',
@@ -19,6 +29,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     MatButtonModule,
     MatNativeDateModule,
     MatDatepickerModule,
@@ -28,38 +39,60 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatSliderModule,
     MatSelectModule,
     MatCheckboxModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatDividerModule
   ],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
 export class Register {
-  formdata = new FormGroup({
-    userName: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    gender: new FormControl('', Validators.required),
-    address: new FormControl(''),
-    birthDate: new FormControl(null, Validators.required),
-    angularSkillLevel: new FormControl(5),
-    country: new FormControl('', Validators.required),          
-    acceptTerms: new FormControl(false, Validators.requiredTrue) 
-  });
+
+  isDarkMode = false;
 
   minSkillLevel = 1;
   maxSkillLevel = 10;
-  submitted = false;    
+
+  countries = ['Philippines', 'USA', 'Canada', 'Australia', 'Other'];
+
+
+  passwordPattern = /^[A-Za-z][A-Za-z0-9]{7,}$/;
+
+  formdata = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.passwordPattern)
+    ]),
+    gender: new FormControl('', Validators.required),
+    address: new FormControl(''),
+    birthDate: new FormControl(null, [
+      Validators.required,
+      this.birthYearValidator
+    ]),
+    country: new FormControl('', Validators.required),
+    angularSkillLevel: new FormControl(5),
+    acceptTerms: new FormControl(false, Validators.requiredTrue)
+  });
+
+  submitted = false;
   submittedData: any = null;
 
-  countries = ['Philippines', 'USA', 'Canada', 'Australia', 'Other']; 
+  birthYearValidator(control: AbstractControl) {
+    if (!control.value) return null;
+
+    const selectedYear = new Date(control.value).getFullYear();
+    return selectedYear <= 2006 ? null : { invalidYear: true };
+  }
 
   onClickSubmit() {
     this.submitted = true;
+
     if (this.formdata.valid) {
       this.submittedData = this.formdata.value;
-      console.log('Form Submitted', this.submittedData);
     } else {
-      console.log('Form Invalid');
       this.submittedData = null;
     }
   }
